93a04d6b1e03999a85e12684f2f61c35f4975d33
Tenkaklet <tenkaklet@gmail.com>
2015-10-29 20:51:20 +0100

    added finishing touches

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 angular.module('madApp', []).controller('firstCtrl', function($scope) {
-    $scope.name = function () {
-            console.log('hello');
-    };
+    $scope.name = 'Name';
+    $scope.location = 'Location';
+    $scope.adjective = 'Adjective';
+    $scope.verb = 'Verb';
+    $scope.adverb = 'Adverb';
 });
