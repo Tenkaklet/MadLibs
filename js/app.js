203d66f1ab3b2b27915779a7b3a099de8154f52c
Tenkaklet <tenkaklet@gmail.com>
2015-11-05 13:42:31 +0100

    almost done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 angular.module('madApp', ['ngMessages']).controller('firstCtrl', function($scope) {
 
+$scope.master = {};
 
     $scope.name = {
         placeholder: 'Add a name',
@@ -29,7 +30,29 @@ angular.module('madApp', ['ngMessages']).controller('firstCtrl', function($scope
         if (isValid) {
             $scope.showText = true;
         } else {
-
+            $scope.showText = false;
         }
+    };
+    $scope.showErrors = true;
+    //reset function
+    $scope.reset = function () {
+        $scope.name = angular.copy($scope.master);
+        // $scope.name.placeholder = angular.copy($scope.name.placeholder);
+        angular.copy($scope.master, $scope.name);
+
+        // to be done later
+        $scope.name.placeholder = 'Add a name';
+        $scope.name.value = '';
+        //
+        $scope.location.placeholder = 'Add a location';
+        $scope.location.value = '';
+
+        $scope.adjective.placeholder = 'Add an adjective';
+        $scope.adjective.value = '';
+        $scope.verb.placeholder = 'Add a verb';
+        $scope.verb.value = '';
+        $scope.adverb.placeholder = 'Add an adverb';
+        $scope.adverb.value = '';
+        $scope.showText = false;
     };
 });
